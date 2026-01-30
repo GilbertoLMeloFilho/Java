4131ef0c25ac34fa88ff6bed03141d619bb7e89e
Gilberto <gilbertolumelofilho@gmail.com>
2026-01-29 23:04:49 -0400

    update

---
+++ Aula01.java
@@ -0,0 +1,18 @@
+//Começo
+public class Aula01 { //Classe geral do bloco principal
+    public static void main(String[] args){ //Bloco de comando vazio e publico
+        System.out.println("Ola Mundo"); //Comando para inprimir (print)
+    }
+}
+/*camelcase  -> uso de maiusculas em palavras juntas -> ExemploDeCamelCase
+---------------------
+MinhaClasse -> Camelcase em ambas é uma classe ou interface
+---------------------
+meuAtributo
+minhaVariavel -> segunda palavra em maiuscula é um atributo, variavel ou metodo
+meuMetodo
+---------------------
+meu_pacote -> sem maiuscula é um pacote
+---------------------
+MINHA_CONSTANTE -> todas maiusculas é uma constante/ ex. PI
+ */
