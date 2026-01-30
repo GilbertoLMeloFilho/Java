b161c43e6dc996b10aa650bf1dfa1eaad64a7caf
Gilberto <gilbertolumelofilho@gmail.com>
2026-01-30 00:49:08 -0400

    update

--- Aula01.java
+++ Aula01.java
@@ -1,7 +1,18 @@
 //Começo
+import java.util.Date; // Importa a biblioteca Date
+import java.util.Locale; // Importa a biblioteca de idioma
+import java.awt.Toolkit;
+import java.awt.Dimension;
 public class Aula01 { //Classe geral do bloco principal
     public static void main(String[] args){ //Bloco de comando vazio e publico
         System.out.println("Ola Mundo"); //Comando para inprimir (print)
+        Date relogio = new Date(); // cria a variavel relogio que recebe a data do dispositivo
+        System.out.println(relogio.toString()); // Imprime a data dentro da variavel relogio
+        Locale idioma = Locale.getDefault();
+        System.out.println(idioma);
+        Toolkit tela = Toolkit.getDefaultToolkit();
+        Dimension dimensao = tela.getScreenSize();
+        System.out.println(dimensao);
     }
 }
 /*camelcase  -> uso de maiusculas em palavras juntas -> ExemploDeCamelCase
@@ -16,3 +27,4 @@ meu_pacote -> sem maiuscula é um pacote
 ---------------------
 MINHA_CONSTANTE -> todas maiusculas é uma constante/ ex. PI
  */
+
